a4996a5591e6df92b3d5f392ed625964aae8c3ed
thomasgriffinx <xytomg@gmail.com>
2016-10-17 11:30:51 +0100

    Master

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Training Cloud</title>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <ul class="right hide-on-med-and-down">
+        <li><a href="../index.html">Home</a></li>
+          <li><a href="../KeyFeatures.html">Guides</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
+        <li>Guides</li>
+    </div>
+  </nav>
+
+    
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center white-text">Training Cloud</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">A Modern Training Solution for Novice Programmers</h5>
+        </div>
+        <div class="row center">
+          <a href="https://developers.openshift.com/getting-started" id="download-button" class="btn-large waves-effect waves-light red lighten-1">Get Started</a>
+        </div>
+        <br><br>
+      </div>
+    </div>
+    <div class="parallax"><img src="../Images/background3.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+
+        <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Tutorials</h5>
+            <p class="light">Learn a new concept and test your knowledge with our interactive coding demos</p>
+          </div>
+        </div>
+     
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Tailored Feedback</h5>
+
+            <p class="light">Providing PaaS based analytics allows users to pin point pain areas like never before</p>
+          </div>
+        </div>
+
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Cloud</h5>
+
+            <p class="light">Cloud technologies allow for user analytics like never before </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <br> </br>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center white-text">Cloud based Training</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Looking to get started?</h5>
+        </div>
+        <div class="row center">
+          <a href="https://developers.openshift.com/getting-started" id="download-button" class="btn-large waves-effect waves-light blue lighten-1">Documentation</a>
+        </div>
+        <br><br>
+      </div>
+    </div>
+    <div class="parallax"><img src="../Images/background2.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  	<div class="section scrollspy" id="work">
+    <div class="container">
+        <h2 class="header text_b">Courses</h2>
+        <div class="row">
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                    <a href="https://itunes.apple.com/album/id1154163561?ls=1&app=itunes">
+                        <img class="activator" src="iTunes.png">
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Java<i class="mdi-navigation-more-vert right"></i></span>
+                        <p><a href="https://itunes.apple.com/album/id1154163561?ls=1&app=itunes"></a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                      <a href="https://www.amazon.co.uk/Save-Me-Socio/dp/B01LZH8WSM/ref=sr_1_1?s=dmusic&ie=UTF8&qid=1476019863&sr=1-1-mp3-albums-bar-strip-0&keywords=save+me+socio">
+                        <img class="activator" src="amazonmusic.png">
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">HTML<i class="mdi-navigation-more-vert right"></i></span>
+                        <p><a href="#"></a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                    <a href="https://play.spotify.com/album/0RCzakXMp1d8WXUmnXtMIb?play=true&utm_source=open.spotify.com&utm_medium=open">
+                        <img class="activator" src="spotify.png">
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Swift<i class="mdi-navigation-more-vert right"></i></span>
+                        <p><a href="#"></a></p>
+                    </div>
+                </div>
+            </div>
+           
+            </div>
+        </div>
+    </div>
+</div>
+
+<br> </br>
+<p class="light">
+
+</p>
+<br> </br>
+</div>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="../js/materialize.js"></script>
+  <script src="../js/init.js"></script>
+
+  </body> 
+
+  
+  <?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+
+   
+        ?> 
+
+</html>
